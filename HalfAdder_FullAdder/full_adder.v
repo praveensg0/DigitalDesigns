85ffc110a2da575e6d5727282d4cc3c4ef3ff96b
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-10-05 13:43:31 +0530

    Create full_adder.v

---
+++ HalfAdder_FullAdder/full_adder.v
@@ -0,0 +1,11 @@
+module full_adder(
+    input a,b,cin,
+    output sum,carry
+    );
+    
+    wire s1,c1,c2;
+    
+    half_adder HA1(a,b,s1,c1);
+    half_adder HA2(s1,cin,sum,c2);
+    assign carry = c1 | c2;
+endmodule
