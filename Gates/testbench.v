61a5bb74281d2b1222df86efb8a71a53f108b545
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-10-13 08:20:53 +0530

    Create testbench.v

---
+++ Gates/testbench.v
@@ -0,0 +1,43 @@
+module tb();
+
+    // DECLARE VARIABLES
+    // input
+    reg A,B;
+    //output
+    wire Y;
+
+    // design call // Module initiation
+    // module_name user_name signals
+    gate dut (
+                .A(A),
+                .B(B),
+                .Y(Y)
+    );
+
+    // Provide Stimulus
+    initial begin
+        A <= 1'b0;
+        B <= 1'b0;
+        #100;
+
+        A <= 1'b0;
+        B <= 1'b1;
+        #100;
+
+        A <= 1'b1;
+        B <= 1'b0;
+        #100;
+
+        A <= 1'b1;
+        B <= 1'b1;
+        #100;
+    end
+
+    // Visualization by using gtkwave 
+
+    // initial begin
+    //     $dumpfile("dump.vcd");
+    //     $dumpvars(0);
+    // end
+
+endmodule
