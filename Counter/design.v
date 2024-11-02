82fe704f2efc65189e0f94f305ffcd6781b0f4d9
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-11-02 17:29:41 +0530

    Update design.v

--- Counter/design.v
+++ Counter/design.v
@@ -1,1 +1,19 @@
+// 4 bit up counter
 
+module counter(clk,rstn,out);
+
+    input clk,rstn;
+    output reg [3:0] out;
+
+    always @ (posedge clk)
+    begin
+        if (rstn == 1'b1)
+        begin
+            out = 4'b0000;
+        end
+        else
+        begin
+            out = out + 1'b1;
+        end
+    end
+endmodule
