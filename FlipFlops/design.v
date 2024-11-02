56f962f1df9c720fafe79a76352baa985d0177e6
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-11-02 17:27:09 +0530

    Update design.v

--- FlipFlops/design.v
+++ FlipFlops/design.v
@@ -1,1 +1,29 @@
+module flop(reset,clk,D,Q);
 
+    // declare inputs
+    input reset, clk, D;
+
+    // declare outputs
+    output reg Q;
+
+    // interim signals 
+
+    // module initiation
+
+    // Logic Design
+    // Procedural blocks
+    // 1. initial block (only testbench)
+    // 2. always block (design + testbench)
+
+    // always @(sensitivity list)
+
+    always @(posedge clk) begin // or negedge clk
+        if(reset == 1'b1) begin
+            Q <= 1'b0;
+        end
+        else begin
+            Q <= D;
+        end
+    end
+    
+endmodule
