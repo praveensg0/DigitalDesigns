f62894e9fc4c41c792c8e4c2c8eb9044ecd945b5
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-11-02 17:28:50 +0530

    Update design.v

--- Decoder/design.v
+++ Decoder/design.v
@@ -1,1 +1,16 @@
+module decoder(A,E,Y_0,Y_1,Y_2,Y_3);
 
+
+input [1:0] A;
+input E;
+
+output Y_0,Y_1,Y_2,Y_3;
+
+// Defining combinational logic for relevant decoder o/p pins
+
+assign Y_0 = (E) & (~A[0]) & (~A[1]);
+assign Y_1 = (E) & (A[0]) & (~A[1]);
+assign Y_2 = (E) & (~A[0]) & (A[1]);
+assign Y_3 = (E) & (A[0]) & (A[1]);
+
+endmodule
