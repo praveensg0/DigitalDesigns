c715fc11f3016e1a9f87d95a8a4918986f05a206
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-11-02 17:25:44 +0530

    Update design.v

--- Mux/design.v
+++ Mux/design.v
@@ -1,1 +1,19 @@
+module mux(A,B,S,Y);
 
+    // Declaring inputs
+    input S;
+    // using A and B as 2 bit input
+    input [1:0] A,B;
+
+    // Declaring output
+    output [1:0] Y;
+
+    // Declaring interim signals
+
+    // Module initiation
+
+    // Logic Design
+    // ternary Operator(like if and else)
+    // (condition) ? first_value : second_value;
+    assign Y = (S == 1'b0) ? A : B;
+endmodule
